67b802aad015c87dc8a345e335a1c3234ed6d846
Nathan Foss <nathanfoss.dev@gmail.com>
2023-05-11 07:34:55 -0500

    Successfully scrape and print all required info

--- scrape_quotes.py
+++ scrape_quotes.py
@@ -5,6 +5,22 @@ url = "https://quotes.toscrape.com/"
 response = requests.get(url)
 doc = BeautifulSoup(response.text, "html.parser")
 # print(doc.prettify())
-quotes = doc.find_all(class_="text")
-for quote in quotes:
-    print(quote.text)
+# quotes = doc.find_all(class_="text")
+quote_containers = doc.find_all(class_="quote")
+for quote_container in quote_containers:
+    print("----------------------------------------")
+    print("Quote:")
+    print(quote_container.find(class_="text").text)
+    print("Author:")
+    print(quote_container.find("span").find_next()
+        .find(class_="author").text)
+    print("Tag(s):")
+    tags = lambda tags: [print(tag.text) for tag in tags]
+    print(tags(quote_container.find_all(class_="tag")))
+    print("URL:")
+    print(url)
+    print()
+
+
+# quote_text = quote.
+
